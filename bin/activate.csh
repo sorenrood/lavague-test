e53fc5538887eb869e49424d0485479da1ed8208
Søren Rood <soren@Srens-MacBook-Air.local>
2024-09-06 11:02:35 +0200

    stuff

--- bin/activate.csh
+++ bin/activate.csh
@@ -9,7 +9,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/soren/Desktop/test-lav"
+setenv VIRTUAL_ENV "/Users/soren/Desktop/lavague-test"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
@@ -18,8 +18,8 @@ setenv PATH "$VIRTUAL_ENV/bin:$PATH"
 set _OLD_VIRTUAL_PROMPT="$prompt"
 
 if (! "$?VIRTUAL_ENV_DISABLE_PROMPT") then
-    set prompt = "(test-lav) $prompt"
-    setenv VIRTUAL_ENV_PROMPT "(test-lav) "
+    set prompt = "(lavague-test) $prompt"
+    setenv VIRTUAL_ENV_PROMPT "(lavague-test) "
 endif
 
 alias pydoc python -m pydoc
--- bin/activate.fish
+++ bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/soren/Desktop/test-lav"
+set -gx VIRTUAL_ENV "/Users/soren/Desktop/lavague-test"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -56,7 +56,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s%s%s" (set_color 4B8BBE) "(test-lav) " (set_color normal)
+        printf "%s%s%s" (set_color 4B8BBE) "(lavague-test) " (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
@@ -65,5 +65,5 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
     end
 
     set -gx _OLD_FISH_PROMPT_OVERRIDE "$VIRTUAL_ENV"
-    set -gx VIRTUAL_ENV_PROMPT "(test-lav) "
+    set -gx VIRTUAL_ENV_PROMPT "(lavague-test) "
 end
